0d954d33b9911e9ec63ea12c81744213b8636ac9
Afaq Khan <afaqk3203@gmail.com>
2024-02-06 10:13:33 +0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -57,7 +57,7 @@ const Header = () => (
       <SocialIcons href="https://github.com/AfaqShahidKhan">
         <AiFillGithub size="3rem" />
       </SocialIcons>
-      <SocialIcons href="https://www.linkedin.com/in/afaq-shahid-102868179/">
+      <SocialIcons href="https://www.linkedin.com/in/afaq-shahid-102868179">
         <AiFillLinkedin size="3rem" />
       </SocialIcons>
       <SocialIcons href="https://twitter.com/afaqshahidkhan">
