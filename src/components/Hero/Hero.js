9cced70bc5745c57b12524c080640cad59756565
AfaqShahidKhan <afaqk3203@gmail.com>
2024-03-06 11:41:30 +0500

    resikve conflicts of hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,11 +21,7 @@ const Hero = () => (
       JSON, NPM, Postman, ActionCable,Turbo-Stream,Ransack, Solidus, jQuery, AJAX, RESTful APIs, Bybug, GIT,
       HTML, CSS,Wordpress, Bootstrap, Material UI, Tailwind CSS, Semantic UI.
       </SectionText>
-<<<<<<< HEAD
       <Button
-=======
-     <Button
->>>>>>> ce97e0796a38f36ba01a299a0c97ece51c5b297e
         onClick={() => {
           window.open("https://www.linkedin.com/in/afaq-shahid-khan", "_blank");
         }}
